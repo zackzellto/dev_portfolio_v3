66ec7a0975ccc1a00602c00af01119df5a6e1a8f
Zack Zellto <zacharyzellto@gmail.com>
2023-05-23 19:05:13 -0600

    started layout for experience card

--- src/App.tsx
+++ src/App.tsx
@@ -25,10 +25,21 @@ function App() {
           <div className="text-center lg:mx-auto lg:-mb-48 lg:mt-40 lg:h-[1px] lg:w-[95%] lg:bg-[#FFFFFF] lg:drop-shadow-md"></div>
           <div>
             <AboutMe />
-          </div>
+          </div>{" "}
+          <div className="text-center sm:-mb-8 md:-mb-32 lg:mx-auto lg:-mb-16 lg:h-[1px] lg:w-[95%] lg:bg-[#FFFFFF] lg:drop-shadow-md"></div>
           <br />{" "}
           <div>
-            <Experience />
+            <Experience
+              company="Intel"
+              position="Senior IT Engineer"
+              dateRange="March 2016 - December 2021"
+              description1="Lead the IT integration & support team for all hardware & software
+              on state-of-the-art replay systems used in all major sporting events."
+              description2="Created a remote support model for live events saving the organization
+              over $100,000+ a year on travel and lodging related expenses."
+              description3="Solved high-level technical issues that helped lead to successful 
+              partnerships with major brands. "
+            />
           </div>
         </div>
       </div>
--- src/components/experience/Experience.tsx
+++ src/components/experience/Experience.tsx
@@ -1,21 +1,53 @@
 import React from "react";
 
-type Props = {};
+type Props = {
+  company: string;
+  position: string;
+  dateRange: string;
+  description1: string;
+  description2: string;
+  description3: string;
+};
 
 const Experience = (props: Props) => {
   return (
-    <div className="m-auto items-center justify-center lg:relative ">
-      <div className="text-center text-[18px] font-bold text-[#00ADB5] drop-shadow-md md:text-center lg:text-center lg:text-[20px] ">
-        <div className="relative top-8 lg:top-56 lg:ml-[54%]">
-          <div className="lg:mr-33 relative right-[107px] top-6 mr-6 text-[12px] text-white lg:right-[120px] ">
-            2.
-          </div>
-          <div className="">Professional Experience</div>
+    <>
+      <div className="m-auto items-center justify-center lg:relative ">
+        <div className="text-center text-[18px] font-bold text-[#00ADB5] drop-shadow-md md:text-center lg:text-center lg:text-[20px] ">
+          <div className="relative top-8 lg:top-24 lg:mr-[54%]">
+            <div className="lg:mr-33 relative right-[107px] top-6 mr-6 text-[12px] text-white lg:right-[120px] ">
+              2.
+            </div>
+            <div className="">Professional Experience</div>
 
-          <div className="mx-auto h-[1px] w-[340px] bg-[#FFFFFF] drop-shadow-md"></div>
+            <div className="mx-auto h-[1px] w-[340px] bg-[#FFFFFF] drop-shadow-md"></div>
+          </div>{" "}
         </div>
       </div>
-    </div>
+      <div className="relative top-32 -z-10 m-auto w-[100%] rounded-xl bg-[#393E46] p-4 text-[14px] text-white shadow-xl drop-shadow-lg lg:top-48 lg:mr-[340px] lg:w-[60%] lg:max-w-[1000px] lg:px-32 xl:mr-[29%]">
+        <div className="mt-8 md:w-[50%] lg:relative lg:left-24 lg:w-[100%]">
+          <div className="text-[18px] font-bold text-[#00ADB5]">
+            {props.company}
+
+            <div className="text-[14px] font-normal text-[#EEEEEE]">
+              {props.position}
+
+              <div className="text-[12px] font-normal text-[#EEEEEE]">
+                {props.dateRange}
+
+                <div className="text-[12px] font-normal text-[#EEEEEE]">
+                  <ul className="relative list-disc">
+                    <li className="pl-2 ">{props.description1}</li>
+                    <li className="pl-2">{props.description2}</li>
+                    <li className="pl-2">{props.description3}</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
