e8d48f29a3b28af8cac7fd78b2b57987dbee9ee6
Zack Zellto <zacharyzellto@gmail.com>
2023-05-29 22:19:59 -0600

    changed mt-4 to mt-16 to get social links working

--- src/components/about-me/AboutMe.tsx
+++ src/components/about-me/AboutMe.tsx
@@ -67,7 +67,7 @@ const TechnologyIcons = () => {
 
 const AboutMe = (props: Props) => {
   return (
-    <div className="m-auto items-center justify-center lg:relative ">
+    <div className="m-auto items-center justify-center lg:relative lg:mt-16 ">
       <div className="text-center text-[18px] font-bold text-[#00ADB5] drop-shadow-md md:text-center lg:text-center lg:text-[20px] ">
         <div className="relative top-8 lg:top-64 lg:ml-[54%]">
           <div className="relative right-14 top-6 mr-6 text-[12px] text-white lg:right-16 ">
