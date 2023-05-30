ed01f89022c5ab37cfab0c57e5162ee239dd72c7
Zack Zellto <zacharyzellto@gmail.com>
2023-05-29 22:26:04 -0600

    added cursor to contact me in footer

--- src/App.tsx
+++ src/App.tsx
@@ -270,7 +270,7 @@ function App() {
               <div className="text-center text-[18px] font-bold text-[#00ADB5] drop-shadow-md md:text-center lg:text-center lg:text-[20px] ">
                 <div className="relative mb-16">
                   <div className="relative right-14 top-6 mr-6 mt-24 text-[12px] text-white lg:right-16 "></div>
-                  <div onClick={handleContactClick} className="">
+                  <div onClick={handleContactClick} className=" cursor-pointer">
                     Contact Me
                   </div>
                   <div className="mx-auto h-[1px] w-[340px] bg-[#FFFFFF] drop-shadow-md"></div>
