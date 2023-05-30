da484112f0859b3dc1bfaf91f467c7f9a781802f
Zack Zellto <zacharyzellto@gmail.com>
2023-05-29 19:11:55 -0600

    resized images on featured projects

--- src/components/portfolio/FeaturedProjectCard.tsx
+++ src/components/portfolio/FeaturedProjectCard.tsx
@@ -18,17 +18,19 @@ const FeaturedProjectCard = (props: Props) => {
   return (
     <div className="mb-16">
       <div className="relative m-auto text-[#00ADB5]">
-        <div className="relative mb-1 text-[14px] lg:ml-[20%]">
+        <div className="relative mb-1 text-[14px] lg:ml-[25%]">
           Featured Project
         </div>
       </div>
 
-      <div className="relative  m-auto w-[100%] rounded-xl bg-[#393E46] p-8 text-[14px] text-white shadow-xl drop-shadow-lg lg:w-[60%]">
-        <img
-          className=" mx-auto mb-4 hidden transform shadow-xl transition-all duration-300 hover:scale-105 sm:block sm:w-3/4 sm:justify-center sm:overflow-hidden sm:opacity-30 sm:hover:opacity-100  md:w-[60%]"
-          src={props.image}
-          alt="project image"
-        />
+      <div className="relative  m-auto w-[100%] rounded-xl bg-[#393E46] p-8 text-[14px] text-white shadow-xl drop-shadow-lg lg:w-[50%]">
+        <div className=" rounded-xl bg-[#00ADB5] drop-shadow-md">
+          <img
+            className=" mx-auto  mb-4 hidden transform shadow-xl transition-all duration-300 hover:scale-105 sm:block sm:justify-center sm:overflow-hidden sm:opacity-30 sm:hover:opacity-100  md:w-[100%]"
+            src={props.image}
+            alt="project image"
+          />
+        </div>
         <div
           className={`mb-8 text-center text-5xl font-bold drop-shadow-lg sm:hidden md:text-6xl lg:text-7xl ${props.fontFamily}`}
         >
